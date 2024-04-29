64c1ce302c4a8c99886af0245a80c3d886485001
ptrcp <116084876+ptrcp@users.noreply.github.com>
2024-04-29 17:44:49 +0700

    Update modelUTS-2.py

--- modelUTS-2.py
+++ modelUTS-2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model = pickle.load('OOPmodel.pkl')
+model = joblib.load('OOPmodel.pkl')
 
 def main():
     st.title('Prediction Model of Churn and Not Churn')
