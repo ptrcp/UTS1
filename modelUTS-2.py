f3438807bc429be0773e76ff7512c61ac65216aa
ptrcp <116084876+ptrcp@users.noreply.github.com>
2024-04-29 18:37:42 +0700

    Update modelUTS-2.py

--- modelUTS-2.py
+++ modelUTS-2.py
@@ -14,12 +14,12 @@ def main():
     st.write('Your area:', geography)
     
     gender = st.radio('Gender', ["Female", "Male"])
-    if gender == '***Female***':
+    if gender == 'Female':
         st.write('You are a female')
     else:
         st.write('You are a male')
     
-    age = st.number_input('Age', value=None)
+    age = st.number_input('Age')
     st.write('You are', age, 'years old')
 
     tenure = st.slider('Tenure', min_value=0, max_value=10, value=1)
